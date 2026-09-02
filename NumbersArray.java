6edb341ca8156fc76ddf73399b00f3526b25dfec
Eunictan <eunice.t.2025@smu.edu.sg>
2026-09-02 20:25:30 +0800

    done

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,45 @@ public class NumbersArray {
     }
 
     // Write your methods here
+    public static int findMax(Integer[] input) {
+        int max = 0;
+        for (int n : input) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        return max;
+    }
+
+    public static Integer[] findDuplicates(Integer[] input) {
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> duplicates = new LinkedHashSet<>();
+
+        for (Integer num : input) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        return duplicates.toArray(new Integer[0]);
+    }
+
+    public static Integer[] findUnique(Integer[] input) {
+        Map<Integer, Integer> countMap = new LinkedHashMap<>();
+
+        for (Integer n : input) {
+            countMap.merge(n, 1, Integer::sum);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result.toArray(new Integer[0]);
+    }
     
 }
 
